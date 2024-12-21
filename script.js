e7b0449a0634a156619293505248745ccd43ca7e
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-12-21 13:32:00 +0300

    dictionary api

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const wrapper = document.querySelector(".wrapper"),
 searchInput = wrapper.querySelector("input"),
 infoText = wrapper.querySelector(".info-text");
 
+function data(result, word){
+    if(result.title){
+        infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+    }else{
+        wrapper.classList.add("active");
+    }
+}
+
 function fetchApi(word){
     infoText.style.color = "#000";
     infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    fetch(url).then(res => res.json()).then(result => console.log(result));
+    fetch(url).then(res => res.json()).then(result => data(result, word));
 }
 
 searchInput.addEventListener("keyup", e =>{
